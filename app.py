af286a09314d0d8b487dd88f23d96a8e63112d1b
yudhiesh <yudhiesh1997@gmail.com>
2021-09-04 11:36:12 +0800

    updated file upload functionality

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import streamlit as st
-import streamlit.components.v1 as stc
+import os
+import requests
 
-import pandas as pd
 import docx2txt
 from PIL import Image
 from PyPDF2 import PdfFileReader
 import pdfplumber
+
+
+def read_tei(tei_file):
+    try:
+        with open(tei_file, "r") as tei:
+            soup = BeautifulSoup(tei, "lxml")
+            return soup
+    except:
+        raise RuntimeError("Cannot generate a soup from the input")
 
 
 def read_pdf(file):
@@ -40,11 +49,11 @@ def main():
         st.subheader("PDF")
         docx_file = st.file_uploader("Upload File", type=["txt", "docx", "pdf"])
         if st.button("Process"):
-            if docx_file is not None:
+            if docx_file:
                 file_details = {
                     "Filename": docx_file.name,
                     "FileType": docx_file.type,
-                    "FileSize": docx_file.size,
+                    "FileSize": f"{round(docx_file.size / 1e6,2)}Mb",
                 }
                 st.write(file_details)
                 # Check File Type
@@ -61,12 +70,25 @@ def main():
                 elif docx_file.type == "application/pdf":
                     # raw_text = read_pdf(docx_file)
                     # st.write(raw_text)
-                    try:
-                        with pdfplumber.open(docx_file) as pdf:
-                            page = pdf.pages[0]
-                            st.write(page.extract_text())
-                    except:
-                        st.write("None")
+                    print(dir(docx_file))
+                    with st.spinner("Processing..."):
+                        try:
+                            pdf_path = os.path.abspath(docx_file.name)
+                            GROBID_URL = (
+                                "http://localhost:8070/api/processFulltextDocument"
+                            )
+                            response = requests.post(
+                                GROBID_URL,
+                                files={"input": open(pdf_path, "rb")},
+                            )
+                            if response.status_code == 200:
+                                st.success("File process successful")
+                            else:
+                                st.error("Error processing file!")
+
+                        except Exception as e:
+                            print(f"{e!r}")
+                            st.error("Error processing file!")
 
                 elif (
                     docx_file.type
